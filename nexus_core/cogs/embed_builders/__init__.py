060fb488d8215c466f7abdfcca20584ddf50604e
Cosmo Chang <cosmo.chang1701@gmail.com>
2026-07-08 18:33:57 +0800

    fix: remove unused import and bump version to 1.9.21

--- nexus_core/cogs/embed_builders/__init__.py
+++ nexus_core/cogs/embed_builders/__init__.py
@@ -109,7 +109,6 @@ from cogs.embed_builders.portfolio_embeds import (
     create_trades_embed,
     create_strategic_dash_embed,
     create_tactical_symbol_embed,
-    create_tactical_uoa_embed,
     create_tactical_hedge_embed,
 )
 
@@ -259,7 +258,6 @@ __all__ = [
     "create_trades_embed",
     "create_strategic_dash_embed",
     "create_tactical_symbol_embed",
-    "create_tactical_uoa_embed",
     "create_tactical_hedge_embed",
     # Watchlist embeds
     "create_watchlist_embed",
